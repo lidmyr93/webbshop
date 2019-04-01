f899661e4e2aead00bcbec437e6ff6a25181bae5
Magnus <magnus.lidmyr@gmail.com>
2019-04-01 13:38:27 +0200

    g-del för köp av en bil klar

--- cars.js
+++ cars.js
@@ -39,26 +39,40 @@ function displayCars(){
     })//getJson
     
 }; //displayCars
-
+/* FUnkar för en bil nu, behöver göra om funktionen att den pushar in infoArray till en annan array på nytt index*/
 function saveCar(){
     let img = $(this).parentsUntil(carholder).children("div").children("img").attr("src");
     let title = $(this).parentsUntil(carholder).children("div").children("h4").text();
     let price = $(this).parentsUntil(carholder).children("div").children("p").last().text();
-
     let infoArray = [img,title,price];
     let jsonInfoArray = JSON.stringify(infoArray);
     localStorage.setItem('order1', jsonInfoArray);
-};
+}
+/* Detta steg är en början på VG-Delen att man kan köpa flera bilar */
+/* let newArray = [];
+function saveCar(){
+    let img = $(this).parentsUntil(carholder).children("div").children("img").attr("src");
+    let title = $(this).parentsUntil(carholder).children("div").children("h4").text();
+    let price = $(this).parentsUntil(carholder).children("div").children("p").last().text();
+    //infoArray kommer ha info om bilen du tryckt på temporärt
+    let infoArray = [img,title,price];
+    console.log(infoArray);
+    //newArray kommer att innehålla arrayer som skapas temporärt av infoArray
+    newArray.push(infoArray);
+    console.log(newArray);
+    let jsonInfoArray = JSON.stringify(newArray);
+    localStorage.setItem('order1', jsonInfoArray);
+}; */
 
 
 
-// funktion för att visa produkten i varukorgen
+// funktion för att visa produkten i varukorgen , OBS denna måste göras om i VG-Delen
 
 function displayOrderedcars(){
     let orderedCars = localStorage.getItem('order1');
     orderedCars = JSON.parse(orderedCars);
+    console.log(orderedCars);
     let varukorg = $("#varukorg-car");
-    console.log(orderedCars[3]);
     if(orderedCars !== null){
         
             varukorg.append(`<div class="card flex-row flex-wrap">
@@ -76,6 +90,7 @@ function displayOrderedcars(){
     }//if
         else{
            //nocar
+           varukorg.append(`<p> Du har inte köpt nån bil än </p>`)
         }//else
     
 };//displayorderedcars
